c838faf625b88d5708003e0775733bd5dc7710f6
rootTimur <timurdogonov99@gmail.com>
2025-11-29 17:02:46 +0400

    Update server.js
    update server.js

--- server.js
+++ server.js
@@ -1,19 +1,18 @@
-// server.js
 const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
-  res.end(`
-    <!DOCTYPE html>
-    <html lang="ru">
-    <body>
-        <img src="https://images.steamusercontent.com/ugc/90472493366823952/9AE061717B44506050E8D1AA5BAD3E51BCAD1185/?imw=5000&imh=5000&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=false">
-    </body>
-    </html>
-  `);
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end(`
+      <!DOCTYPE html>
+      <html lang="ru">
+      <body>
+          <img src="https://images.steamusercontent.com/ugc/90472493366823952/9AE061717B44506050E8D1AA5BAD3E51BCAD1185/?imw=5000&imh=5000&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=false">
+      </body>
+      </html>`);
 });
 
-const PORT = process.env.PORT || 8080;
-server.listen(PORT, () => {
-  console.log(`Сервер запущен на порту ${PORT}`);
+server.listen(8080, () => {
+    console.log('🚀 Сервер запущен: http://localhost:8080');
 });
